79e179af22186f5b05ac3e5fc75f2f0f81074d05
artosepyan <artyomosepyan@mail.ru>
2025-04-18 18:41:12 +0300

    Fixed PR's badge

--- update_pr_count.py
+++ update_pr_count.py
@@ -1,17 +1,17 @@
 import requests
 import os
 
-# Замените на ваш username
-USERNAME = os.getenv('GITHUB_USERNAME') or "artengin"
+USERNAME = "artengin"
 URL = f"https://api.github.com/search/issues?q=is:pr+is:merged+author:{USERNAME}"
 
 response = requests.get(URL).json()
-count = response["total_count"]
+count = response.get("total_count", 0)
+
+print(f"🔍 Найдено PR: {count}")
 
 with open("README.md", "r") as f:
     readme = f.read()
 
-# Обновляем значение в README (например, ищем строку `![PRs]`)
 updated_readme = readme.replace(
     "![PRs](https://img.shields.io/badge/Merged_PRs-0-blue)",
     f"![PRs](https://img.shields.io/badge/Merged_PRs-{count}-blue)"
